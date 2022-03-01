5f9a6f144db6ba1855655699256efc38725f9ef9
withnahidul <nahidulislam6800@gmail.com>
2022-03-01 17:42:13 +0600

    update-1

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const displaySearchResult = data => {
     const searchResult = document.getElementById('search-result');
     data.forEach(data => {
         //console.log(data);
+
+        // <button onclick="showDetails()" class="btn btn-outline-secondary" type="button">Details</button>
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
@@ -26,7 +28,7 @@ const displaySearchResult = data => {
             <h5 class="card-title">${data.phone_name}</h5>
             <h6 class="card-title">${data.brand}</h6>
             <p class="card-text"> This content is a little bit longer.</p>
-                <button onclick="showDetails()" class="btn btn-outline-secondary" type="button">Details</button>
+            <a href="${data.slug}" class="btn btn-primary">Go somewhere</a>
         </div>
     </div>
         `;
@@ -42,4 +44,14 @@ const loadPhoneDetails = mainFeatures => {
 }
 const displayDetail = data => {
     console.log(data);
+    const mainFeatures = document.getElementById('features-details');
+    div.classList.add('card');
+    div.innerHTML = `
+    <img src="${data.image}" class="card-img-top" alt="...">
+    <div class="card-body">
+      <h5 class="card-title">${data.mainFeatures}</h5>
+      <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+      <a href="${data.mainFeatures}" class="btn btn-primary">Go somewhere</a>
+    </div>
+    `;
 }
